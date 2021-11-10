dda47997422617e3824642e3bc9ec86e2be128b7
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-10 14:52:44 +0000

    correct the binary

--- _get_binary.c
+++ _get_binary.c
@@ -9,14 +9,28 @@
 
 int _get_binary(va_list args)
 {
-	unsigned int b = va_arg(args, int);
+	unsigned int b = va_arg(args, unsigned int);
+	int len, i;
+	char *str_binary = NULL;
 
-	if (b != 0)
+	if (b == 0)
+		return (_putchar('0'), 1);
+
+	str_binary = malloc(sizeof(char) * 32);
+
+	for (i = 0; b != 0; i++)
 	{
-		return (_print_binary(b));
+		str_binary[i] = b % 2 + '0';
+		b /= 2;
 	}
-	else
-		_putchar('0');
 
-	return (1);
+	for (len = 0; str_binary[len]; len++)
+		;
+
+	for (len--; str_binary[len]; len--)
+		_putchar(str_binary[len]);
+
+	free(str_binary);
+
+	return (i);
 }
--- _print_binary.c
+++ _print_binary.c
@@ -9,14 +9,11 @@
 
 int _print_binary(unsigned int b)
 {
-	int length = 0;
-
 	if (b)
 	{
-		length++;
 		_print_binary(b / 2);
 		_putchar(b % 2 + '0');
 	}
 
-	return (length);
+	return (1);
 }
